10518ef70f3eeb392a22175cfe8a1cbadc2bd012
arya <elprab17@gmail.com>
2023-08-24 13:56:59 +0700

    update tugas 4

--- 4_time-complexity-and-space-complexity/praktikum/soal1/bilanganPrima.go
+++ 4_time-complexity-and-space-complexity/praktikum/soal1/bilanganPrima.go
@@ -3,29 +3,47 @@ package main
 import "fmt"
 
 
-func prima(a int){
+func primeNumber(a int) bool{
+	isPrime := false
 	if a > 1 {
 		switch a {
 			case 2:
-				fmt.Printf("Bilangan Prima") 
+				// fmt.Printf("Bilangan Prima") 
+				isPrime = true
 				break
 			case 3:
-				fmt.Printf("Bilangan Prima") 
+				// fmt.Printf("Bilangan Prima") 
+				isPrime = true
 				break
 			default :
-				if a % 2 == 0 || a % 3 == 0 {
-					fmt.Printf("Bukan Bilangan Prima")
-					return
+				if a % 2 == 0 || a % 3 == 0 || a % 7 == 0 {
+					// fmt.Printf("Bukan Bilangan Prima")
+					break
 				}
-				fmt.Printf("Bilangan Prima")
+				isPrime = true
+				// fmt.Printf("Bilangan Prima")
 		}
-		return
 	}
-	fmt.Printf("Bukan Bilangan Prima")
+	return isPrime
+	// fmt.Printf("Bukan Bilangan Prima")
 }
 
-func main(){
-	var bilangan int
-	fmt.Scanln(&bilangan)
-	prima(bilangan)
-}
+// func main(){
+// 	var bilangan int
+// 	fmt.Scanln(&bilangan)
+// 	prima(bilangan)
+// }
+
+func main() {
+
+	fmt.Println(primeNumber(1000000007)) // true
+	
+	fmt.Println(primeNumber(13)) // true
+	
+	fmt.Println(primeNumber(17)) // true
+	
+	fmt.Println(primeNumber(20)) // false
+	
+	fmt.Println(primeNumber(35)) // false
+	
+	}
--- 4_time-complexity-and-space-complexity/praktikum/soal2/pangkat.go
+++ 4_time-complexity-and-space-complexity/praktikum/soal2/pangkat.go
@@ -7,35 +7,21 @@ package main
 
 import "fmt"
 
-func pangkat1(a float64, b int) float64 {
+func pow(a float64, b int) float64 {
 	var hasil float64 = 1
 	if b >= 0 {
-		// for i := 1.0; i <= b; i++ {
-		// 	hasil = hasil*a
-		// }
 		for b > 0 {
 			if b%2 == 1 {
 				hasil *= a
-				fmt.Println(hasil)
 			}
 			a *= a
-			fmt.Println(hasil, a)
 			b/=2
 		}
 	}else {
-		// for i := b; i<0; i++ {
-		// 	hasil = hasil/a
-		// 	fmt.Printf("%s %.16f\n","hasil : ", hasil)
-		// }
 		for b < 0 {
-			// fmt.Println("b :",b)
 			if b%2 == -1{
 				hasil /= a
 			}
-			// else {
-			// 	hasil /= a
-			// 	fmt.Println("hasil2 :", hasil)
-			// }
 			a *= a
 			b/=2
 		}
@@ -44,40 +30,14 @@ func pangkat1(a float64, b int) float64 {
 }
 
 func main(){
-	var bilangan float64
-	var pangkat int
 
-	fmt.Printf("Masukkan nilai bilangan : ")
-	fmt.Scanln(&bilangan)
-	
-	fmt.Printf("Masukkan nilai pangkat : ")
-	fmt.Scanln(&pangkat)
-	
-	fmt.Println(pangkat1(bilangan, pangkat))
+	fmt.Println(pow(2, 3)) // 8
+
+	fmt.Println(pow(5, 3)) // 125
+
+	fmt.Println(pow(10, 2)) // 100
+
+	fmt.Println(pow(2, 5)) // 32
+
+	fmt.Println(pow(7, 3)) // 343
 }
-
-// import "strconv"
-
-// func main(){
-// 	var x, n int
-// 	var input string
-// 	var err error
-
-// 	fmt.Printf("masukkan nilai x : ")
-// 	fmt.Scanln(&input)
-// 	x, err = strconv.Atoi(input)
-// 	if err != nil {
-// 		fmt.Println("nilai harus angka")
-// 		return
-// 	}
-	
-// 	fmt.Printf("masukkan nilai n : ")
-// 	fmt.Scanln(&input)
-// 	n, err = strconv.Atoi(input)
-// 	if err != nil {
-// 		fmt.Println("nilai harus angka")
-// 		return
-// 	}
-
-// 	fmt.Println()
-// }
