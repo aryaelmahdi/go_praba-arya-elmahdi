496af0517a4689644a45f5dac967e53259a358c9
arya <elprab17@gmail.com>
2023-11-15 17:54:34 +0700

    chore(database):add db config

---
+++ 20_clean-architecture/praktikum/utils/database/db_config.go
@@ -0,0 +1,18 @@
+package database
+
+import (
+	"tugas/praktikum/model"
+
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+func ConnectDB() (*gorm.DB, error) {
+	return gorm.Open(sqlite.Open("app.db"), &gorm.Config{})
+}
+
+func MigrateDB(db *gorm.DB) error {
+	return db.AutoMigrate(
+		model.User{},
+	)
+}
